4e88e807669b38b291998e9a5ed96b3acb3ea7fd
Steve Howell <showell30@yahoo.com>
2019-11-06 11:31:41 -0500

    Initial commit.

---
+++ README.md
@@ -0,0 +1,11 @@
+This project helps you parser JSON into a custom type
+with things like DFloat, DInt, and DList.
+
+It is very early stages!!!!
+
+The eventual goal is to help you generate Elm code
+from JSON payloads.  It will read the data, infer the
+structure, and generate sample code for things like
+decoders.
+
+It is intended as a DEVELOPMENT TOOL ONLY.
---
+++ src/Main.elm
@@ -0,0 +1,71 @@
+module Main exposing (main)
+
+import Browser
+import DParser
+import Html
+
+
+
+-- MODEL / INIT
+
+
+type alias Model =
+    { title : String
+    }
+
+
+type alias Msg =
+    Never
+
+
+main : Program () Model Msg
+main =
+    Browser.document
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
+
+
+init : () -> ( Model, Cmd Msg )
+init _ =
+    let
+        model =
+            { title = "DataUtil Demo"
+            }
+    in
+    ( model, Cmd.none )
+
+
+
+-- UPDATE
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update _ model =
+    ( model, Cmd.none )
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    Sub.none
+
+
+
+-- VIEW
+
+
+view : Model -> Browser.Document Msg
+view model =
+    let
+        _ =
+            Debug.log "example" (DParser.parse "10.2")
+    in
+    { title = model.title
+    , body = [ Html.text "see debugger" ]
+    }
